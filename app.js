2330c5208786a2e6f94b1fef7536d63e524063f1
Arjun K V <arjunkvarju7@gmail.com>
2022-09-15 12:05:01 +0530

    first commit kcc website

---
+++ app.js
@@ -0,0 +1,49 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var hbs = require('express-handlebars')
+var db = require('./configure/connection')
+
+var adminRouter = require('./routes/admin');
+var membersRouter = require('./routes/members');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutDir:__dirname+'/views/layout/',partialDir:__dirname+"/views/partials/"}))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+db.connect((err,done)=>{
+  if(err)console.log("Database connection failed" +err)
+  else console.log("Databse conneted succesfully")
+})
+
+app.use('/', membersRouter);
+app.use('/admin', adminRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ configure/connection.js
@@ -0,0 +1,24 @@
+const mongoClient = require("mongodb").MongoClient
+
+const state = {
+    db:null
+}
+
+module.exports = {
+    connect : function (done){
+        const url = "mongodb://localhost:27017";
+        const dbname = "kss" ;
+
+        mongoClient.connect(url,(err,data)=>{
+            if(err)return done(err)
+
+            else{
+                state.db = data.db(dbname)
+                done()
+            }
+        })
+    },
+    get : function(){
+        return state.db
+    }
+}
